c6c1d0c1f0a92e298377ff1097f7d6518ec40475
Manjeet Kumar <manjeet.kmr18@gmail.com>
2025-07-24 23:44:55 -0400

    docker updated

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 const { profileController } = require('./controllers/profileController');
 const { reviewsScraperController } = require('./controllers/reviewsScraperController');
